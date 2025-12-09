bd3b633b9f9ef0af7c391febf0c7cb7d4b5efd26
ziedbenboubaker <zied.ghouili27@gmail.com>
2025-12-09 11:56:45 +0100

    Fix warning port in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  // أضف هذا الجزء الخاص بالإعدادات التجريبية
+  experimental: {
+    serverActions: {
+      // قم بإضافة الـ IP الذي ظهر في التحذير مع المنفذ (Port)
+      allowedOrigins: ['192.168.32.1:3000', 'localhost:3000'],
+    },
+  },
 };
 
 export default nextConfig;
